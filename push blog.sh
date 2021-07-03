fb1d601c60ed916af11626225315b5a1dc996bc9
imzhang95 <imzhang95@gmail.com>
2021-07-03 17:53:12 +0800

    20210703

--- push blog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git add .
-git commit -m "2021-07-03"
-git push -u origin develop
-
-exit 0
---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git add .
+git commit -m "$(date +%Y%m%d)"
+# git push -u origin develop
+
+exit 0
